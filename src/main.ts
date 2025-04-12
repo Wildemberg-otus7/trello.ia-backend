52bc8c00873c12cc4e2a08b2bab39df3f3187668
Wildemberg-otus7 <wildemberg.oliveira@otus7.com>
2025-04-12 18:11:24 -0300

    feat(cors): habilitar CORS com configurações específicas no bootstrap

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,12 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.FRONTEND_URL,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
